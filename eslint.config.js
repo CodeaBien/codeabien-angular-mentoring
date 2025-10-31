cb1a2fd2f9cac75129e230de3ba9f5ad2c06561a
Jimy Dolores <jimmyhd_19@hotmail.com>
2025-10-30 21:36:12 -0500

    hola dios, soy yo de nuevo

--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,7 @@ module.exports = tseslint.config(
 					style: 'kebab-case',
 				},
 			],
-			// '@angular-eslint/prefer-inject': 'warn',
+			'@angular-eslint/prefer-inject': 'warn',
 		},
 	},
 	{
