c8b5e6976e72fc0f8ee55d34aef3ba595a3c2e84
Alvaro Mena Santos <alvaro.mena@encora.com>
2025-11-04 15:51:07 -0500

    feat: file with lefthook

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -23,7 +23,7 @@ import { AuthStore } from '@/features/auth/domain/store/auth.store';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {
-	// Dashboard component
+	// Dashboard component demo
 	constructor(
 		private authStore: AuthStore,
 		private router: Router
