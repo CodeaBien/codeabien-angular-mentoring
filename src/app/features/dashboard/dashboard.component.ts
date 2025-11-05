86cc75f69b2c842bdc3f27f5212a7c253a3e8baa
Alvaro Mena Santos <alvaro.mena@encora.com>
2025-11-04 20:35:09 -0500

    feat: test with jobs and run into lefthook file and npm run

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthStore } from '@/features/auth/domain/store/auth.store';
 
